3fc31d9eaf820fd0d8966343d16f3a9d657f753d
kimmin-young <kimmin-young@218.38.137.8>
2015-11-30 17:42:25 +0900

    미

--- src/com/hanains/network/test/Nslookup.java
+++ src/com/hanains/network/test/Nslookup.java
@@ -6,23 +6,23 @@ import java.util.Scanner;
 public class Nslookup {
 	public static void main(String[] args) throws Exception{
 		InetAddress ns =null;
-		boolean flag=true;
-		
 		Scanner sc = new Scanner(System.in);
-		while(flag){
+		
+		while(true){
 			System.out.println(">");
 			String host = sc.nextLine();
-			if(host=="exit"){
-				flag = false;
-				System.out.println("Nslookup 종료");
-//			}
-//			if("exit".equals(host)){
-//				break;
-//			} 강사님
-			
+			//			if(host=="exit"){
+			//				flag = false;
+			//				System.out.println("Nslookup 종료");
+			//			}내가 첨에 생각한거 근데안됫음..
+			if("exit".equals(host)){
+				System.out.println("끝");
+				break;
+			}//강사님꺼 
+
 			InetAddress res = ns.getByName(host);
 			System.out.println(res.toString());
-			}
 		}
 	}
+
 }
