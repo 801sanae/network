f19548aacd3ecf837c83d9aaa72f5d0a31f038e8
bit-user <bit-user@bit>
2015-12-01 09:13:39 +0900

    .

--- src/com/hanains/network/test/Nslookup.java
+++ src/com/hanains/network/test/Nslookup.java
@@ -4,27 +4,29 @@ import java.net.*;
 import java.util.Scanner;
 
 public class Nslookup {
-	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(System.in);
-		
+	public static void main(String[] args) {
 		while(true){
+			Scanner sc = new Scanner(System.in);
+			
 			System.out.println(">");
 			String host = sc.nextLine();
-			//			if(host=="exit"){
-			//				flag = false;
-			//				System.out.println("Nslookup 종료");
-			//			}내가 첨에 생각한거 근데안됫음..
-			if("exit".equals(host)){
+
+			if("exit".equals(host) && host!=null){
 				System.out.println("끝");
 				break;
-			}//강사님꺼 
-
-			InetAddress[] ns = InetAddress.getAllByName(host);
-			
-			for (int i = 0; i < ns.length; i++) {
-				System.out.println(ns[i].toString());
 			}
+
+			InetAddress[] ns;
 			
+			try {
+				ns = InetAddress.getAllByName(host);
+				
+				for (int i = 0; i < ns.length; i++) {
+					System.out.println(ns[i].toString());
+				}
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
